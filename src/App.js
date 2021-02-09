9ed65e9d8aaf133520890c617ca7894d7f193cd4
Renzo Gaspary <rgaspary@gmail.com>
2021-02-09 15:51:40 -0500

    Creating calendar module

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import React from 'react';
+
+import Calendar from './components/calendar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Miniature Octo Meme</h1>
+        <p>A calendar application to keep track of bills and budgets</p>
       </header>
+      <h2>Components</h2>
+      <div className="component-container">
+        <h3>Calendar Component</h3>
+        <Calendar/>
+      </div>
     </div>
   );
 }
